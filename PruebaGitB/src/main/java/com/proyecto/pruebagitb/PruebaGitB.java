50136e787bafaad012503c96687beb71d55d7f59
cronceroicloud <croncero@icloud.com>
2024-02-15 08:30:44 +0100

    Commit 1: Práctica realizada 1

---
+++ PruebaGitB/src/main/java/com/proyecto/pruebagitb/PruebaGitB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.pruebagitb;
+
+/**
+ *
+ * @author daw1_
+ */
+public class PruebaGitB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
